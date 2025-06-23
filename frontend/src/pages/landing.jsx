3b039f97c7ed961df8cb7ed43c2a1c49bd84fd41
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-06-23 23:09:19 +0530

    Add. Creating Landing/Home Page

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import "../App.css"
+import {Link} from 'react-router-dom'
 
 export default function landing() {
   return (
-    <div>
-      LandingPage
+    <div className='landingPageContainer'>
+        <nav>
+            <div className='navHeader'>
+            <h2>Zoom Clone</h2>
+        </div>
+        <div className='navlist'>
+            <p>Join as Guest</p>
+            <p>Register</p>
+            <div role='button'>
+                <p>Login</p>
+            </div>
+        </div>
+        </nav>
+
+        <div className="landingMainContainer">
+            <div>
+                <h1><span style={{color:"#42aaf5"}}>Connect</span> with the World!</h1>
+                <p>Cover the distance by Zoom</p>
+                <div>
+                    <h5 style={{color:"#42aaff"}}>Get Started</h5>
+                </div>
+            </div>
+            <div>
+                <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/1*yogb8pbd0pUunXAEM3fJaw.png" alt="Image" />
+            </div>
+        </div> 
+
     </div>
   )
 }
