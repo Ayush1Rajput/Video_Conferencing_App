69c8641960f29ab3ac32275417e7d4817d2b12b0
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-06-25 21:55:25 +0530

    Add. handling the routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import LandingPage from './pages/landing';
+import Authentication from './pages/authentication';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <LandingPage/>
+      <Router>
+        <Routes>
+         
+            <Route path='/' element={<LandingPage />} />
+
+            <Route path='/auth' element={<Authentication />} />
+        </Routes>
+      </Router>
+
     </>
   );
 }
