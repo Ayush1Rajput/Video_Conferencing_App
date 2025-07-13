20412710ad982ea7b394693a0295e0a1640a5c75
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-07-13 17:36:42 +0530

    Add app.use user routes

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,8 @@ app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
+app.use("/api/v1/users", userRoutes);
+
 const start = async () => {
   try {
     const connectionDb = await mongoose.connect(process.env.MONGO_URL);
