54e67ad2b0186b6bb30d097ed1dad3c8ec136796
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-07-16 22:08:29 +0530

    Add. Start working on Sockets

--- backend/controllers/socketManager.js
+++ backend/controllers/socketManager.js
@@ -1,7 +1,31 @@
-import { Server } from "socket.io"
+import { Server, Socket } from "socket.io"
+
+let connections = {};
+let messages = {}
+let timeOnline = {};
 
 const connectToSocket = (server)=>{
     const io = new Server(server);
+
+    io.on("connection",(socket)=>{
+
+        socket.on("join-call",(path)=>{
+
+        });
+
+        socket.on("signal",(toId,message)=>{
+            io.to(toId).emit("signal",socket.id, message);
+        });
+
+        socket.on("chat-message",(data,sender)=>{
+
+        })
+
+        socket.on("disconnect",()=>{
+            
+        })
+    })
+
     return io;
 }
 
