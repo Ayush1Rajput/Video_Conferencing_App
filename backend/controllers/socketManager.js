27b34e47aae83d3950494613866dc363c818f3f3
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-07-16 22:39:58 +0530

    Handle user disconnect and broadcast 'user-left' event

--- backend/controllers/socketManager.js
+++ backend/controllers/socketManager.js
@@ -61,6 +61,29 @@ const connectToSocket = (server)=>{
         })
 
         socket.on("disconnect",()=>{
+            
+            var diffTime = Math.abs(timeOnline[socket.id]-new Date())
+
+            var key;
+
+            for(const[k,v] of JSON.parse(JSON.stringify(Object.entries(connections)))){
+                
+                for(let a=0;a<v.length;++a){
+                    if(v[a]===socket.id){
+                        key=k
+                        for(let i=0;i<connections[key].length;++a){
+                            io.to(connections[key][i]).emit('user-left',socket.id);
+                        }
+
+                        var index = connections[key].indexOf(socket.id)
+                        connections[key].splice(index,1)
+
+                        if(connections[key].length===0){
+                            delete connections[key];
+                        }
+                    }
+                }
+            }
 
         })
     })
