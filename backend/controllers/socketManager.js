03ce0cdb56f967a91553cf99284ba55473de2040
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-07-16 22:21:41 +0530

    Add user join-call handling and message sync on connection

--- backend/controllers/socketManager.js
+++ backend/controllers/socketManager.js
@@ -10,7 +10,25 @@ const connectToSocket = (server)=>{
     io.on("connection",(socket)=>{
 
         socket.on("join-call",(path)=>{
+            if(connections[path]=== undefined){
+                connections[path]=[]
+            }
 
+            connections[path].push(socket.id)
+
+            timeOnline[socket.id]= new Date();
+
+            for(let a=0;i<connections[path].length;a++){
+                io.to(connections[path][a]).emit('user-joined',socket.id,connections[path]);
+            }
+
+
+            if(messages[path]!==undefined){
+                for(let a=0;a<messages[path].length;++a){
+                    io.to(socket.id).emit("chat-message",messages[path][a]['data'],
+                        messages[path][a]['sender'], messages[path][a]['socket-id-sender'])
+                }
+            }
         });
 
         socket.on("signal",(toId,message)=>{
@@ -22,7 +40,7 @@ const connectToSocket = (server)=>{
         })
 
         socket.on("disconnect",()=>{
-            
+
         })
     })
 
