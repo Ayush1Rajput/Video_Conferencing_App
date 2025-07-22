67a1bfa0b26f0bda3f2f0f22026b272425177e39
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-07-22 22:22:30 +0530

    Try login and register pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,21 @@
-import './App.css';
-import LandingPage from './pages/landing';
-import Authentication from './pages/authentication';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import "./App.css";
+import LandingPage from "./pages/landing";
+import Authentication from "./pages/authentication";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
     <>
       <Router>
-        <Routes>
-         
-            <Route path='/' element={<LandingPage />} />
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
 
-            <Route path='/auth' element={<Authentication />} />
-        </Routes>
+            <Route path="/auth" element={<Authentication />} />
+          </Routes>
+        </AuthProvider>
       </Router>
-
     </>
   );
 }
