faafc3c7008f4a09805d0871fc4c1dcd951ac5f2
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-07-30 21:28:13 +0530

    Add: Start working on Video Meet Page

---
+++ frontend/src/pages/VideoMeet.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const server_url  = "http://localhost:8000";
+
+const connections = [];
+
+export default function VideoMeetComponent(){
+    return (
+        <div>VideoMeet Components</div>
+    )
+}
