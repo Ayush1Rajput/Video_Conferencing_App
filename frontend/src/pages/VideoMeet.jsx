91433638dee63e7907d6fc8fa6c51aa716719498
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-08-12 18:17:34 +0530

    Test the work

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -351,7 +351,13 @@ export default function VideoMeetComponent() {
           </div>
         </div>
       ) : (
-        <></>
+        <>
+          <video ref={localVideoref} autoPlay muted></video>
+          
+          {videos.map((video)=>(
+            <div key={video.socketId}></div>
+          ))}
+        </>
       )}
     </div>
   );
