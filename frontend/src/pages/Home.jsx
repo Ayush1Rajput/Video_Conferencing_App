2143ed73175b82e6241ad6a00cb1d92ffab20998
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-08-21 14:53:54 +0530

    Implement video call navigation

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import withAuth from '../utils/withAuth'
+import { Navigate, useNavigate } from 'react-router-dom'
 
 function HomeComponent() {
+
+  let navigate = useNavigate();
+  const [meetingCode, setMeetingCode] = useState("");
+
+  let handleJoinVideoCall = async () =>{
+    navigate(`/${meetingCode}`);
+  }
+
   return (
     <div>
       
