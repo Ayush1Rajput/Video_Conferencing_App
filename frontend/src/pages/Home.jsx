4a5c2295540f06747e9502ebc8f0060541730a0f
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-08-23 10:37:11 +0530

    Creating Navbar on Home component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,21 +1,51 @@
-import React, { useState } from 'react'
-import withAuth from '../utils/withAuth'
-import { Navigate, useNavigate } from 'react-router-dom'
+import React, { useContext, useState } from "react";
+import withAuth from "../utils/withAuth";
+import { useNavigate } from "react-router-dom";
+import "../App.css";
+import { Button, IconButton, TextField } from "@mui/material";
+import RestoreIcon from "@mui/icons-material/Restore";
+import { AuthContext } from "../contexts/AuthContext";
 
 function HomeComponent() {
-
+  
   let navigate = useNavigate();
   const [meetingCode, setMeetingCode] = useState("");
 
-  let handleJoinVideoCall = async () =>{
+  const { addToUserHistory } = useContext(AuthContext);
+  let handleJoinVideoCall = async () => {
+    await addToUserHistory(meetingCode);
     navigate(`/${meetingCode}`);
-  }
+  };
 
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div className="navBar">
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <h2>Apna Video Call</h2>
+        </div>
+
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <IconButton
+            onClick={() => {
+              navigate("/history");
+            }}
+          >
+            <RestoreIcon />
+          </IconButton>
+          <p>History</p>
+
+          <Button
+            onClick={() => {
+              localStorage.removeItem("token");
+              navigate("/auth");
+            }}
+          >
+            Logout
+          </Button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default withAuth(HomeComponent);
