0e95730764fa4095c11180815d411b75970f438a
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-08-24 11:57:36 +0530

    Fetch and display user meeting history on component mount

--- frontend/src/pages/history.jsx
+++ frontend/src/pages/history.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
 
 export default function history() {
-  return (
-    <div>
-      
-    </div>
-  )
+  const { getHistoryOfUser } = useContext(AuthContext);
+
+  const [meetings, setMeetings] = useState([]);
+
+  useEffect(() => {
+    const fetchHistory = async () => {
+      try {
+        const history = await getHistoryOfUser();
+        setMeetings(history);
+      } catch {
+        // IMPLEMENT SNACKBAR
+      }
+    };
+
+    fetchHistory();
+  }, []);
+
+  return <div>
+
+  </div>;
 }
