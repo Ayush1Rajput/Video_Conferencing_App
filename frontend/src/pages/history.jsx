27d88711a814aefd4e16bfceaf75a93e8c08eaaf
Ayush1Rajput <ayushraj1rajput@gmail.com>
2025-08-24 12:32:39 +0530

    fetch and display user meeting history with error handling

--- frontend/src/pages/history.jsx
+++ frontend/src/pages/history.jsx
@@ -1,37 +1,65 @@
-import React, { useContext } from "react";
-import { AuthContext } from "../contexts/AuthContext";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../contexts/AuthContext.jsx";
+import { useNavigate } from "react-router-dom";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import HomeIcon from "@mui/icons-material/Home";
+import { IconButton } from "@mui/material";
 
-import Card from '@mui/material/Card';
-
-export default function history() {
+export default function History() {
   const { getHistoryOfUser } = useContext(AuthContext);
-
   const [meetings, setMeetings] = useState([]);
+  const routeTo = useNavigate();
 
   useEffect(() => {
     const fetchHistory = async () => {
       try {
         const history = await getHistoryOfUser();
         setMeetings(history);
-      } catch {
-        // IMPLEMENT SNACKBAR
+      } catch (error) {
+        // Implement Snackbar 
+        console.error("Error fetching meetings:", error);
       }
     };
 
     fetchHistory();
   }, []);
 
-  return (<div>
-    {
-        meetings.map(e=>{
-            return(
-                <>
-                 <Card key={i} variant="outlined">
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const year = date.getFullYear();
 
-                 </Card>
-                 </>
-            )
-        })
-    }
-  </div>);
+    return `${day}/${month}/${year}`;
+  };
+
+  return (
+    <div>
+      <IconButton onClick={() => routeTo("/home")}>
+        <HomeIcon />
+      </IconButton>
+      {meetings.length !== 0 ? (
+        meetings.map((e, i) => (
+          <Card key={i} variant="outlined">
+            <CardContent>
+              <Typography
+                sx={{ fontSize: 14 }}
+                color="text.secondary"
+                gutterBottom
+              >
+                Code: {e.meetingCode}
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                Date: {formatDate(e.date)}
+              </Typography>
+            </CardContent>
+          </Card>
+        ))
+      ) : (
+        <Typography>No meetings found</Typography>
+      )}
+    </div>
+  );
 }
